8bf42be46a5da7b52cda577e76303dd7e335e0be
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2021-02-14 22:09:16 +0530

    working switch case construct

---
+++ Syntax_Validation_and_Token_Generation/test9.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<compilerdesign.h>
+class 
+// is this a good code or not 
+double a = 3.1415e+3;
+int arr[5];
+
+int fun(){
+	int i=0;
+	while(i>=0){
+		if(i==0){
+			int j;
+			int i = 10;
+			j = 20;
+			k = i+j;
+			
+			while(k>=0)
+				k=k-1;
+		}
+		else if(i>0){
+			int k;
+		}
+		else{
+			int l; 
+			while (--j < 20);
+		}
+	}
+	switch(a)
+	{
+	case 1:  b = c + d;
+		j = 90;
+	case 2: c = d + e;
+	case 3: 
+		while ( i++ < 10)
+			{
+				switch (i)
+				{
+					case 1: p= 20;
+						j = 90;
+					case 3:
+						--p;
+						break;
+
+					default:
+						h = i / k;
+						break;
+				}
+			}
+	default: break;
+	}
+}
