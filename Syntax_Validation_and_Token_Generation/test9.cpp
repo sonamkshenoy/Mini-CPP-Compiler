bdeedb0225e447f3936ebcd6e1443f3186729888
Ansh Sarkar <52290294+anshsarkar@users.noreply.github.com>
2021-02-14 17:54:49 +0530

    Working lexer and parser without switch and class

---
+++ Syntax_Validation_and_Token_Generation/test9.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<compilerdesign.h>
+class 
+// is this a good code or not 
+double a = 3.1415e+3;
+int arr[5];
+
+int fun(){
+	int i=0;
+	while(i>=0){
+		if(i==0){
+			int j;
+			int i = 10;
+			j = 20;
+			k = i+j;
+			
+			while(k>=0)
+				k=k-1;
+		}
+		else if(i>0){
+			int k;
+		}
+		else{
+			int l; 
+		}
+	}
+}
